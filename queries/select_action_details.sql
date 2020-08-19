ff90a03e52fa7c51b54fe01425215bb6e8752d1e
Luke Towell <luke.towell@hotmail.com>
2020-08-19 16:25:29 +0100

    added storeitem by category query
    - added the storeItem by category query

--- queries/select_action_details.sql
+++ queries/select_action_details.sql
@@ -5,3 +5,12 @@ store_item_action a
 INNER JOIN status b
 on a.status_id=b.status_id 
 WHERE user_id = 'lmt0013';
+
+
+-- select item_action details using storename and categoryId --
+
+SELECT si.id, si.item_upc, si.store_id, i.name, i.category_id, i.img_url 
+FROM store_item si
+INNER JOIN item i 
+ON si.item_upc = i.upc 
+WHERE i.category_id = 1 AND si.store_id ='4128'
