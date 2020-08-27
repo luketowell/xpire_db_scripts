65ee2746c43c08111e7914404cf9880e887313a4
Luke Towell <luke.towell@hotmail.com>
2020-08-27 11:06:38 +0100

    Updated table schemas
    - updated the table schemas

--- create/create_tables.sql
+++ create/create_tables.sql
@@ -2,40 +2,42 @@
 
 CREATE TABLE store (
 	id VARCHAR(5) PRIMARY KEY,
-	name VARCHAR(100)
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE item_category (
 	id INT IDENTITY PRIMARY KEY,
-	name VARCHAR(100)
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE status (
 	id INT IDENTITY PRIMARY KEY,
-	name VARCHAR(100)
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE item (
 	upc VARCHAR(20) PRIMARY KEY,
-	name VARCHAR(100),
-	description VARCHAR(2048),
-	expiry DATE,
-	category_id INT,
-	size VARCHAR(55),
+	name VARCHAR(100) NOT NULL,
+	description VARCHAR(2048) NOT NULL,
+	category_id INT NOT NULL,
+	size VARCHAR(55) NOT NULL,
 	img_url VARCHAR(1024)
 );
 
 CREATE TABLE store_item_action (
 	action_id INT IDENTITY PRIMARY KEY,
-	store_item_id INT,
-	user_id VARCHAR(20),
-	status_id INT,
-	expired_count INT,
-	update_date DATETIME
+	store_item_id INT NOT NULL,
+	user_id VARCHAR(20) NOT NULL,
+	status_id INT NOT NULL,
+	expired_count INT NOT NULL,
+	created_date DATETIME NOT NULL
 );
 
 CREATE TABLE store_item (
 	id INT IDENTITY PRIMARY KEY,
-	item_upc VARCHAR(20),
-	store_id VARCHAR(5)
+	item_upc VARCHAR(20) NOT NULL,
+	store_id VARCHAR(5) NOT NULL,
+	expiry_date DATE NOT NULL,
+	created_date DATE NOT NULL,
+	updated_date DATE
 );
